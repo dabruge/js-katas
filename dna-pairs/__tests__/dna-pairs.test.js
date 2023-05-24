650a9bcc5ff53471666ee5435b530a4578c519b9
dabruge <davidbrucegeddes@gmail.com>
2023-05-24 14:12:48 +0100

    Complete dnaPairs function and tests

--- dna-pairs/__tests__/dna-pairs.test.js
+++ dna-pairs/__tests__/dna-pairs.test.js
@@ -1,7 +1,14 @@
 const dnaPairs = require('../dna-pairs.js');
 
 describe('dnaPairs()', () => {
-  test('ADD YOUR TEST DESCRIPTION HERE', () => {
-    // add your test assertion(s) here
+  test('should return an empty array when passed empty string', () => {
+    expect(dnaPairs("")).toEqual([])
+  });
+  test('should return array containing one pair array when passed single character', () => {
+    expect(dnaPairs("G")).toEqual([ ["G", "C"] ])
+  });
+  test('should return nested array of correct pairs when passed string of 2 or more length', () => {
+    expect(dnaPairs("AG")).toEqual([ ["A", "T"], ["G", "C"] ])
+    expect(dnaPairs("ACGT")).toEqual([ ["A", "T"], ["C", "G"], ["G", "C"], ["T", "A"] ])
   });
 });
--- dna-pairs/dna-pairs.js
+++ dna-pairs/dna-pairs.js
@@ -1,6 +1,23 @@
 // Please do not change the name of this function
 function dnaPairs(dna) {
-  // Write your code here
+
+  if (dna.length === 0) return []
+  
+  const dnaSplit = dna.split("")
+  
+  const pairs = dnaSplit.map(char => {
+    switch(char) {
+      case "A":
+        return Array.from(char + "T")
+      case "T":
+        return Array.from(char + "A")
+      case "C":
+        return Array.from(char + "G")
+      case "G":
+        return Array.from(char + "C")
+    }
+  })
+  return pairs
 }
 
 module.exports = dnaPairs;
