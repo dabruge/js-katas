638b426338368ca482bbdbbbc99a75ba41f2e507
dabruge <davidbrucegeddes@gmail.com>
2023-05-24 16:57:11 +0100

    Add pseudo code for change calculator

--- change-calculator/change-calculator.js
+++ change-calculator/change-calculator.js
@@ -1,6 +1,14 @@
 // Please do not change the name of this function
 function changeCalculator (num) {
-  // Your code here
+  // create empty object
+  // define coins array (in pence) [200, 100, 50, 20, 10, 5, 2, 1]
+
+  // iterate through coins array
+    // check num / coin and floor it
+    // if 1 or more
+      // create new key:value in object - coin from array: floored division result
+    // set num to modulo of num and current array coin
+  // return object
 };
 
 module.exports = changeCalculator;
